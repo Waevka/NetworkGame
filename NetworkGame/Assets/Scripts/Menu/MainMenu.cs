5113c004d77b107d2923a6502880af9b9cf0c265
OutBast <barltomiej.wardzinski@gmail.com>
2018-01-06 12:21:16 +0100

    Substitution of UI in Main scene. Don't need to put address or login etc. Just click connnect to server & create server.

--- NetworkGame/Assets/Scripts/Menu/MainMenu.cs
+++ NetworkGame/Assets/Scripts/Menu/MainMenu.cs
@@ -1,10 +1,25 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.UI;
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+
+public class UserInfo
+{
+    public UserInfo(string login, string pass)
+    {
+        m_Login = login;
+        m_Password = pass;
+    }
+
+    public string m_Login;
+    public string m_Password;
+}
 
 public class MainMenu : MonoBehaviour
 {
+    public TestNetworkScript network;
+
     public GameObject m_MainMenu;
     public GameObject m_ClientMenu;
     public GameObject m_ServerMenu;
@@ -22,6 +37,7 @@ public class MainMenu : MonoBehaviour
 
     void Awake()
     {
+        network = FindObjectOfType<TestNetworkScript>();
         DisableCurrentMenu(ref m_ClientMenu);
         DisableCurrentMenu(ref m_ServerMenu);
         SetCurrentMenu(ref m_MainMenu);
@@ -58,23 +74,58 @@ public class MainMenu : MonoBehaviour
 
     public void CreateServerButtonAction()
     {
-
+        network.ButtonCreateServer();
+        DisableCurrentMenu(ref m_ServerMenu);
     }
 
     public void ConnectButtonAction()
     {
-        if (m_ServerAddress.text.Length == 0)
+        string input_login = m_CLogin.text;
+        string input_password = m_CPassword.text;
+        string input_addr = m_ServerAddress.text;
+        string input_port = m_Port.text;
+
+        if (File.Exists("users.txt"))
         {
-            Debug.Log(m_ServerAddress.placeholder.GetComponent<Text>().text);
+            List<UserInfo> database_login = new List<UserInfo>();
+            string[] lines = File.ReadAllLines("users.txt");
+            foreach (string line in lines)
+            {
+                string[] uinfo = line.Split(',');
+                database_login.Add(new UserInfo(uinfo[0], uinfo[1]));
+            }
+
+            foreach (UserInfo info in database_login)
+            {
+                if (input_login == info.m_Login && input_password == info.m_Password)
+                {
+                    Debug.Log("jestem");
+                }
+            }
         }
-        else
-        {
-            Debug.Log(m_ServerAddress.text);
-        }
+
+        network.ButtonConnectToServer();
+        DisableCurrentMenu(ref m_ClientMenu);
+
+        //if (m_ServerAddress.text.Length == 0)
+        //{
+        //    Debug.Log(m_ServerAddress.placeholder.GetComponent<Text>().text);
+        //}
+        //else
+        //{
+        //    Debug.Log(m_ServerAddress.text);
+        //}
     }
 
     public void AddUserButtonAction()
     {
-
+        StreamWriter sw = new StreamWriter("users.txt", true);
+        if (m_SLogin.text.Length != 0 && m_SPassword.text.Length != 0)
+        {
+            sw.WriteLine(m_SLogin.text + "," + m_SPassword.text);
+            m_SLogin.text = "";
+            m_SPassword.text = "";
+        }
+        sw.Close();
     }
 }
